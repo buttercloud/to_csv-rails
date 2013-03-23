a5468945bc5a6f569fbebd2565439d9193e25ff3
Feras Hirzalla <hirzalla@gmail.com>
2013-03-23 16:40:24 +0200

    Support custom headers with the :headings argument

--- lib/to_csv-rails.rb
+++ lib/to_csv-rails.rb
@@ -22,7 +22,7 @@ class Array
     
     data = []
     # header
-    data << columns.map(&:to_s).map(&:humanize).join(', ') if options[:header]
+    data << (options[:headings] || columns.map(&:to_s).map(&:humanize)).join(', ') if options[:header]
 
     self.each do |obj|
       data << columns.map{ |column| obj.send(column) }.join(', ')
